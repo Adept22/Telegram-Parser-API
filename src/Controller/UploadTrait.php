cd7e3dd8433218305801ef541ee7b8c7e77bf0b4
Vladislav Terenchuk <asdof71@yandex.ru>
2022-03-30 18:54:40 +0300

    Import fix

--- src/Controller/UploadTrait.php
+++ src/Controller/UploadTrait.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
-use Webmozart\Assert\Assert;
+use Symfony\Component\Validator\Constraints as Assert;
 
 trait UploadTrait
 {
