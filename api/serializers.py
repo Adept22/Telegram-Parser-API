0ae3b084b01e32627418ee7540b060dec0fa3ec5
Vladislav Terenchuk <asdof71@yandex.ru>
2022-06-24 10:35:24 +0300

    ChatTask serializer fix

--- api/serializers.py
+++ api/serializers.py
@@ -197,5 +197,5 @@ class TaskListSerializer(serializers.ModelSerializer):
     class Meta:
         model = base_models.Task
         fields = "__all__"
-        read_only_fields = ("id", "created_at", "started_at", "ended_at", "status", "status_text")
+        read_only_fields = ("id", "created_at")
 
